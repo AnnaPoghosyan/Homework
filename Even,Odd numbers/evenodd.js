0c31e28ec6785ca0c19e5d4eee75ad3111092c99
AnnaPoghosyan <32462397+AnnaPoghosyan@users.noreply.github.com>
2020-03-20 00:54:07 +0400

    Checking even and odd numbers

---
+++ Even,Odd numbers/evenodd.js
@@ -0,0 +1,10 @@
+let num = +prompt("Enter the number");
+   if (num === 0) {
+      console.log(num);
+   }
+   else if (num % 2 === 0) {
+   console.log(num);   
+   }
+   else {
+   document.write(num);
+   }
